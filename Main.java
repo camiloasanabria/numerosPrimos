1a90353c32cfa192c4b917f0497f5b904f3e4011
Unknown <cass465andres@gmail.com>
2018-02-15 14:35:20 -0500

    a,b numeros primos

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main{
+	static void DeterminarNumeroPrimo(int numero,boolean primo){
+		for(int i=2;i<numero;i++){
+			if((numero%i)==0){
+				System.out.println(numero+" No es numero primo");
+				i=numero;
+				primo=false;
+			}
+		}
+		if(primo==true){
+			System.out.println(numero+" Es numero primo");
+		}
+	}
+	public static void main(String[] arg){
+		int a=5,b=10;
+		boolean primo=true;
+		DeterminarNumeroPrimo(a,primo);
+		primo=true;
+		DeterminarNumeroPrimo(b,primo);
+	}
+}
